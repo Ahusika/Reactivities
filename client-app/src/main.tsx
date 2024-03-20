e6c83418a6a70f982de1b7fab2cdf0e099216259
husein.alikadic <husein.alikadic@gmail.com>
2024-03-20 10:18:01 +0100

    Routing

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import { StoreContex, store } from './app/stores/store'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/Routes'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreContex.Provider value={store}>
-      <App />
+      <RouterProvider router={router}/>
     </StoreContex.Provider>
   </React.StrictMode>,
 )

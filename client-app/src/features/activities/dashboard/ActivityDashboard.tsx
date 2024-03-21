c6bd7650f80d3b23268ef5f61f318209714abf6b
husein.alikadic <husein.alikadic@gmail.com>
2024-03-22 00:18:01 +0100

    section 9, stayling uhe User Interface

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,17 +2,18 @@
 import { Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
+import ActivityFilters from "./ActivityFilters";
 import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function ActivityDashboard() { 
-    const {activityStore} = useStore();
-    const {loadActivities, activityRegistry} = activityStore;
+  const { activityStore } = useStore();
+  const { loadActivities, activityRegistry } = activityStore;
 
     useEffect(() => {
         if(activityRegistry.size <= 1) loadActivities();
-    }, [activityRegistry.size, loadActivities])
+    }, [loadActivities, activityRegistry.size])
 
 
   if (activityStore.loadingInitial) return <LoadingComponent  content="Loading app"/>
@@ -22,7 +23,7 @@ export default observer(function ActivityDashboard() {
               <ActivityList />
             </Grid.Column>
             <Grid.Column width="6">
-               <h2>Activity filters</h2>
+            <ActivityFilters />
             </Grid.Column>
         </Grid>
     )
